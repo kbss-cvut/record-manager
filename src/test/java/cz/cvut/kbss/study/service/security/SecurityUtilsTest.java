6122737705bae52ffb3b0573a323aba1de4f117c
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2023-10-20 10:29:40 +0200

    [Ref] Refactor SecurityUtilsTest.

--- src/test/java/cz/cvut/kbss/study/service/security/SecurityUtilsTest.java
+++ src/test/java/cz/cvut/kbss/study/service/security/SecurityUtilsTest.java
@@ -5,46 +5,48 @@ import cz.cvut.kbss.study.environment.util.Environment;
 import cz.cvut.kbss.study.model.Institution;
 import cz.cvut.kbss.study.model.PatientRecord;
 import cz.cvut.kbss.study.model.User;
-import cz.cvut.kbss.study.service.BaseServiceTestRunner;
-import cz.cvut.kbss.study.service.InstitutionService;
-import cz.cvut.kbss.study.service.PatientRecordService;
-import cz.cvut.kbss.study.service.UserService;
+import cz.cvut.kbss.study.persistence.dao.PatientRecordDao;
+import cz.cvut.kbss.study.persistence.dao.UserDao;
+import cz.cvut.kbss.study.util.IdentificationUtils;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
+public class SecurityUtilsTest {
 
-public class SecurityUtilsTest extends BaseServiceTestRunner {
+    @Mock
+    private UserDao userDao;
 
-    @Autowired
-    private SecurityUtils securityUtils;
+    @Mock
+    private PatientRecordDao patientRecordDao;
 
-    @Autowired
-    private UserService userService;
-
-    @Autowired
-    private InstitutionService institutionService;
-
-    @Autowired
-    private PatientRecordService patientRecordService;
+    @InjectMocks
+    private SecurityUtils sut;
 
     private User user;
-    public static final String USERNAME = "halsey";
-    public static final String PASSWORD = "john117";
+
+    public static final String USERNAME = "username";
+    public static final String PASSWORD = "pass" + Generator.randomInt(0, 1000);
 
     @BeforeEach
     public void setUp() {
         Institution institution = Generator.generateInstitution();
-        institutionService.persist(institution);
-        this.user = Generator.getUser(USERNAME, PASSWORD, "John", "Johnie", "Johnie@gmail.com", institutionService.findByName(institution.getName()));
+        institution.setKey(IdentificationUtils.generateKey());
+        this.user = Generator.getUser(USERNAME, PASSWORD, "John", "Johnie", "Johnie@gmail.com", institution);
         user.generateUri();
-        userService.persist(user);
     }
 
     @AfterEach
@@ -55,69 +57,73 @@ public class SecurityUtilsTest extends BaseServiceTestRunner {
     @Test
     public void getCurrentUserReturnsCurrentlyLoggedInUser() {
         Environment.setCurrentUser(user);
-        final User result = securityUtils.getCurrentUser();
+        when(userDao.findByUsername(user.getUsername())).thenReturn(user);
+        final User result = sut.getCurrentUser();
         assertEquals(user, result);
     }
 
     @Test
-    public void isMemberOfInstitutionReturnsMembershipStatusTrue() {
+    public void isMemberOfInstitutionReturnsTrueForUserFromSpecifiedInstitution() {
         Environment.setCurrentUser(user);
-        assertTrue(securityUtils.isMemberOfInstitution(user.getInstitution().getKey()));
+        when(userDao.findByUsername(user.getUsername())).thenReturn(user);
+        assertTrue(sut.isMemberOfInstitution(user.getInstitution().getKey()));
     }
 
     @Test
-    public void isMemberOfInstitutionReturnsMembershipStatusFalse() {
+    public void isMemberOfInstitutionReturnsFalseForDifferentInstitutionKey() {
         Environment.setCurrentUser(user);
-        assertFalse(securityUtils.isMemberOfInstitution("nonExistingInstitutionKey"));
+        when(userDao.findByUsername(user.getUsername())).thenReturn(user);
+        assertFalse(sut.isMemberOfInstitution("nonExistingInstitutionKey"));
     }
 
     @Test
-    public void areFromSameInstitutionReturnsMembershipStatusTrue() {
+    public void areFromSameInstitutionReturnsTrueForUserFromSameInstitution() {
         Environment.setCurrentUser(user);
+        when(userDao.findByUsername(user.getUsername())).thenReturn(user);
 
         User userFromSameInstitution = Generator.generateUser(user.getInstitution());
-        userService.persist(userFromSameInstitution);
+        when(userDao.findByInstitution(user.getInstitution())).thenReturn(List.of(user, userFromSameInstitution));
 
-        assertTrue(securityUtils.areFromSameInstitution(userFromSameInstitution.getUsername()));
+        assertTrue(sut.areFromSameInstitution(userFromSameInstitution.getUsername()));
     }
 
     @Test
-    public void areFromSameInstitutionReturnsMembershipStatusFalse() {
+    public void areFromSameInstitutionReturnsFalseForUserFromDifferentInstitution() {
         Environment.setCurrentUser(user);
+        when(userDao.findByUsername(user.getUsername())).thenReturn(user);
 
         Institution institutionAnother = Generator.generateInstitution();
-        institutionService.persist(institutionAnother);
 
         User userFromAnotherInstitution = Generator.generateUser(institutionAnother);
-        userService.persist(userFromAnotherInstitution);
-        assertFalse(securityUtils.areFromSameInstitution(userFromAnotherInstitution.getUsername()));
+        when(userDao.findByInstitution(user.getInstitution())).thenReturn(List.of(user));
+
+        assertFalse(sut.areFromSameInstitution(userFromAnotherInstitution.getUsername()));
     }
 
     @Test
-    public void isRecordInUsersInstitutionReturnsMembershipStatusTrueForUser() {
+    public void isRecordInUsersInstitutionReturnsTrueWhenRecordBelongsToCurrentUsersInstitution() {
         Environment.setCurrentUser(user);
+        when(userDao.findByUsername(user.getUsername())).thenReturn(user);
 
         PatientRecord record = Generator.generatePatientRecord(user);
+        record.setKey(IdentificationUtils.generateKey());
+        when(patientRecordDao.findByKey(record.getKey())).thenReturn(record);
 
-        patientRecordService.persist(record);
-        assertTrue(securityUtils.isRecordInUsersInstitution(record.getKey()));
+        assertTrue(sut.isRecordInUsersInstitution(record.getKey()));
     }
 
     @Test
-    public void isRecordInUsersInstitutionReturnsMembershipStatusFalse() {
+    public void isRecordInUsersInstitutionReturnsFalseWhenRecordBelongsToInstitutionDifferentFromCurrentUsers() {
         Environment.setCurrentUser(user);
-
+        when(userDao.findByUsername(user.getUsername())).thenReturn(user);
         Institution institutionAnother = Generator.generateInstitution();
-        institutionService.persist(institutionAnother);
-
+        institutionAnother.setKey(IdentificationUtils.generateKey());
         User userFromAnotherInstitution = Generator.generateUser(institutionAnother);
-        userService.persist(userFromAnotherInstitution);
 
         PatientRecord record = Generator.generatePatientRecord(userFromAnotherInstitution);
-        patientRecordService.persist(record);
+        record.setKey(IdentificationUtils.generateKey());
+        when(patientRecordDao.findByKey(record.getKey())).thenReturn(record);
 
-        Environment.setCurrentUser(userFromAnotherInstitution);
-
-        assertFalse(securityUtils.isRecordInUsersInstitution(record.getKey()));
+        assertFalse(sut.isRecordInUsersInstitution(record.getKey()));
     }
 }
