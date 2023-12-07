b8e4d0c9852832cc2006127a05598a97b3970f9a
Miroslav Blasko <blcham@gmail.com>
2023-12-07 08:37:20 +0100

    [Upd] Remove constraint for User to be part of Institution

--- src/main/java/cz/cvut/kbss/study/model/User.java
+++ src/main/java/cz/cvut/kbss/study/model/User.java
@@ -56,7 +56,7 @@ public class User implements HasDerivableUri, Serializable {
     @OWLDataProperty(iri = Vocabulary.s_p_created)
     private Date dateCreated;
 
-    @ParticipationConstraints(nonEmpty = true)
+//    @ParticipationConstraints(nonEmpty = true)
     @OWLObjectProperty(iri = Vocabulary.s_p_is_member_of, fetch = FetchType.EAGER)
     private Institution institution;
 
